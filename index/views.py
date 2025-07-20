53e3a7dd87112b9567156f894aeb74739d6e7879
Zhelyazko Dedov <109466030+zhel13@users.noreply.github.com>
2025-07-21 01:35:55 +0300

    updating code into index file

--- index/views.py
+++ index/views.py
@@ -3,22 +3,21 @@ from django.views.generic import TemplateView, ListView
 from traveler.utils import get_traveler
 from trips.utils import get_trips
 
-class AllTrips(ListView):
+class ContextMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['traveler'] = get_traveler()
+        return context
+
+class AllTrips(ContextMixin, ListView):
     queryset = get_trips()
     context_object_name = 'trips'
     template_name = 'all-trips.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['traveler'] = get_traveler()
-        return context
 
 
-class IndexView(TemplateView):
+
+class IndexView(ContextMixin ,TemplateView):
     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['traveler'] = get_traveler()
-        return context
 
